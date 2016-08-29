705f57a0cb0f54788a032b0dd3c3b220af693ef1
ZackSungy <2043193520@qq.com>
2016-08-29 10:27:20 +0800

    Factorial

---
+++ Factorial（递归公式）/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+long f(int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n*f(n - 1);
+}
+int main()
+{
+	int n;
+	long y;
+	do{
+		printf("请输入大于等于零的数：");
+		scanf("%d", &n);
+	} while (n < 0);
+	y = f(n);
+	printf("%d!=%ld", n, y);
+	return 0;
+}
