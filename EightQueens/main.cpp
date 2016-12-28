a509be6c9e54447e48ae357cf51566857bb2799c
ZackSungy <2043193520@qq.com>
2016-12-28 16:26:07 +0800

    malgorithm

--- EightQueens/main.cpp
+++ EightQueens/main.cpp
@@ -27,4 +27,45 @@ int main()
 	search(0);
 	printf("%d", tot);
 	return 0;
+}// input total
+
+
+
+
+
+
+
+#include<stdio.h>
+
+int n, tot = 0, C[100];
+
+void search(int cur)
+{
+	int i, j;
+	if (cur == n){
+		for (i = 0; i < n; i++)
+			printf("%d ", C[i]);
+		printf("\n");
+	}
+	else for (i = 0; i < n; i++)
+	{
+		int ok = 1;
+		C[cur] = i;
+		for (j = 0; j < cur; j++)
+		if (C[cur] == C[j] || cur - C[cur] == j - C[j] || cur + C[cur] == j + C[j])
+		{
+			ok = 0;
+			break;
+		}
+		if (ok)search(cur + 1);
+	}
 }
+
+
+int main()
+{
+	scanf("%d", &n);
+	search(0);
+	printf("%d", tot);
+	return 0;
+}//input array
