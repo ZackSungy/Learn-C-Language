b053be41cb1a6084fee4ec904114f8dffe98aab0
ZackSungy <2043193520@qq.com>
2017-02-08 11:55:01 +0800

    algorithm

--- MaximumMinimization/main.cpp
+++ MaximumMinimization/main.cpp
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
-int n, m, Maximum, A[1000];
+int n, m, Maximum, A[1000], c;
 
 int Judge(int Maximum, int *A)
 {
 	int sum = 0;
 	int pa = 0;
+	int pa2 = 0, j = 0, k = 0, sum1 = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += A[i];
@@ -13,7 +14,29 @@ int Judge(int Maximum, int *A)
 		{
 			sum = A[i];
 			pa++;
+			if (pa == m - 2)
+				k = i;
+			if (pa == m - 1)
+				j = i;
 		}
+	}
+	for (int i = j; i < n; i++)
+	{
+		sum1 += A[i];
+		if (sum1>Maximum)
+		{
+			pa2++;
+			break;
+		}
+	}
+	if (pa == m - 1 && pa2 == 0)
+	{
+		int sum1 = 0, sum2 = 0;
+		for (k; k <= j; k++)
+			sum1 += A[k];
+		for (j; j < n; j++)
+			sum2 += A[j];
+		c = sum1>sum2 ? sum2 : sum1;
 	}
 	if (pa >= m)
 		return 0;
@@ -23,7 +46,7 @@ int Judge(int Maximum, int *A)
 
 int Half_Stack(int low, int hige, int *A)
 {
-	if (low >= hige)
+	if (low > hige)
 		return hige + 1;
 	int mid = (low + hige) / 2;
 	if (Judge(mid, A) == 1)
@@ -46,9 +69,11 @@ int main()
 			max = A[i];
 	}
 	a = Half_Stack(min, sum, A);
-	if (a>max)
-		printf("%d", a);
+	if (max>a)
+		a = max;
+	if (c > a)
+		printf("%d\n", c);
 	else
-		printf("%d", max);
+		printf("%d\n", a);
 	return 0;
 }
--- QuickSort/main.cpp
+++ QuickSort/main.cpp
@@ -1,39 +1,54 @@
-#include <stdio.h>  
-int i, j, c;
+#include<stdio.h>
 
-void quick_sort(int left, int right, int *A)
+int n, m, Maximum, A[1000];
+
+int Judge(int Maximum, int *A)
 {
-	if (left >= right)
-		return;
-	int i = left, j = right;
-	int key = A[left];
-	while (i < j)
+	int sum = 0;
+	int pa = 0;
+	for (int i = 0; i < n; i++)
 	{
-		while (i < j&&key <= A[j])
-			j--;
-		A[i] = A[j];
-		while (i < j&&key >= A[i])
-			i++;
-		A[j] = A[i];
-		c++;
+		sum += A[i];
+		if (sum>Maximum)
+		{
+			sum = A[i];
+			pa++;
+		}
 	}
-	A[i] = key;
-	quick_sort(left, i - 1, A);
-	quick_sort(i + 1, right, A);
+	if (pa >= m)
+		return 0;
+	else
+		return 1;
 }
 
+int Half_Stack(int low, int hige, int *A)
+{
+	if (low >= hige)
+		return hige + 1;
+	int mid = (low + hige) / 2;
+	if (Judge(mid, A) == 1)
+		return Half_Stack(low, mid - 1, A);
+	else
+		return Half_Stack(mid + 1, hige, A);
+}
 
 int main()
 {
-	int A[100], n;
-	scanf("%d", &n);
+	int sum = 0, max = -1, min = 100000, a;
+	scanf("%d%d", &n, &m);
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &A[i]);
+		sum += A[i];
+		if (min>A[i])
+			min = A[i];
+		if (max < A[i])
+			max = A[i];
 	}
-	quick_sort(0, n - 1, A);
-	for (int i = 0; i < n; i++)
-		printf("%d ", A[i]);
-	printf("\n");
+	a = Half_Stack(min, sum, A);
+	if (a>max)
+		printf("%d", a);
+	else
+		printf("%d", max);
 	return 0;
 }
