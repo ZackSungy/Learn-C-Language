44437b0a202a7516f79484d31fa008c0812340a3
ZackSungy <2043193520@qq.com>
2017-02-16 18:09:01 +0800

    algorithm

--- CoinQuestion/main.cpp
+++ CoinQuestion/main.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 
-int n, V[101], INF = 1000000000;
+int INF = 100000000, n, V[101];
 
 void print_ans(int *d, int S)
 {
-	for (int i = 0; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	if (S >= V[i] && d[S] == d[S - V[i]] + 1)
 	{
 		printf("%d ", V[i]);
@@ -21,24 +21,24 @@ int main()
 	{
 		memset(min, 0, sizeof(min));
 		memset(max, 0, sizeof(max));
+		memset(V, 0, sizeof(V));
 		for (int i = 1; i <= n; i++)
-			scanf("%d", V + i);
-		min[0] = max[0] = 0;
+			scanf("%d", &V[i]);
 		for (int i = 1; i <= S; i++)
 		{
-			min[i] = INF; max[i] = -INF;
+			min[i] = INF;
+			max[i] = -INF;
 		}
-		for (int i = 1; i <= S; i++)
+		for (int i = 0; i <= S; i++)
 		for (int j = 1; j <= n; j++)
 		if (i >= V[j])
 		{
-			min[i] = min[i] < min[i - V[j]] + 1 ? min[i] : min[i - V[j]] + 1;
-			max[i] = max[i] > max[i - V[j]] + 1 ? max[i] : max[i - V[j]] + 1;
+			min[i] = min[i]<min[i - V[j]] + 1 ? min[i] : min[i - V[j]] + 1;
+			max[i] = max[i]>max[i - V[j]] + 1 ? max[i] : max[i - V[j]] + 1;
 		}
 		printf("%d %d\n", min[S], max[S]);
 		print_ans(min, S);
 		printf("\n");
 		print_ans(max, S);
 	}
-	return 0;
 }
